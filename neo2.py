fd094b7130c8cfd69263dc5f03066d4a5f9c1ea3
Kaifan Song <202200502068@stumail.sztu.edu.cn>
2024-06-02 00:07:18 +0800

    Add files via upload

---
+++ neo2.py
@@ -0,0 +1,274 @@
+import os
+from models.passage import Passage
+from contextlib import contextmanager
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_core.documents import Document
+from langchain_experimental.graph_transformers import LLMGraphTransformer
+from langchain_community.graphs import Neo4jGraph
+from neo4j import GraphDatabase
+from langchain_community.vectorstores import Neo4jVector
+from langchain_core.runnables import (
+    RunnableBranch,
+    RunnableLambda,
+    RunnableParallel,
+    RunnablePassthrough,
+)
+from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts.prompt import PromptTemplate
+from langchain_core.pydantic_v1 import BaseModel, Field
+from typing import Tuple
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+import os
+from langchain_community.graphs import Neo4jGraph
+from langchain.text_splitter import TokenTextSplitter
+from langchain_openai import ChatOpenAI
+from langchain_experimental.graph_transformers import LLMGraphTransformer
+from neo4j import GraphDatabase
+from langchain_community.vectorstores import Neo4jVector
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores.neo4j_vector import remove_lucene_chars
+from langchain_core.runnables import  RunnableParallel, RunnablePassthrough
+import re
+import os
+from neo4j import GraphDatabase
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
+from models.passage import Passage
+from langchain_core.documents import Document
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from templates2 import prompt
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import (
+    RecursiveCharacterTextSplitter,
+)
+from langchain.graphs.graph_document import (
+    Node as BaseNode,
+    Relationship as BaseRelationship
+)
+from typing import List
+from langchain.pydantic_v1 import Field, BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_experimental.graph_transformers import LLMGraphTransformer
+from langchain_community.graphs import Neo4jGraph
+from langchain.text_splitter import TokenTextSplitter
+
+
+os.environ["NEO4J_URI"] = "bolt://localhost:7687"
+os.environ["NEO4J_USERNAME"] = "neo4j"
+os.environ["NEO4J_PASSWORD"] = "skf707=="
+
+graph = Neo4jGraph()
+
+AI_KEY = "sk-Swi6dHHVWDY342vVaCwFLwmguz6YXfVlSXAfNxzukMtsScfP"
+AI_URL = "https://api.chatanywhere.tech/v1"
+
+os.environ["OPENAI_API_KEY"] = AI_KEY
+os.environ["OPENAI_API_BASE"] = AI_URL
+
+engine = create_engine("mysql+pymysql://root:Sztu2024!@nj-cdb-ejzzmfxj.sql.tencentcdb.com:63911/crawler", echo=True)
+Session = sessionmaker(bind=engine)
+
+neo4j_driver = GraphDatabase.driver(
+    os.environ["NEO4J_URI"],
+    auth=(os.environ["NEO4J_USERNAME"], os.environ["NEO4J_PASSWORD"])
+)
+
+def delete_all_nodes():
+    with neo4j_driver.session() as session:
+        session.run("MATCH (n) DETACH DELETE n")
+        
+model_name = input("input the model name: ")
+if model_name == "GPT3":
+    llm = ChatOpenAI(model="gpt-3.5-turbo-16k", temperature=0)
+elif model_name == "GPT4":
+    llm = ChatOpenAI(model="gpt-4", temperature=0)
+llm_transformer = LLMGraphTransformer(llm=llm)
+
+@contextmanager
+def scoped_session():
+        db_session = Session()
+        try:
+            yield db_session
+        finally:
+            db_session.close() 
+            
+content_ids = input("Input the IDs of the content (separate IDs by space): ").strip().split()
+contents = []
+with scoped_session() as conn:
+    for content_id in content_ids:
+        result = conn.query(Passage.content).offset(int(content_id) - 1).limit(1).all()
+        content = result[0][0] if result else None
+        if content:
+            contents.append(content)
+        
+llm_transformer_filtered = LLMGraphTransformer(
+    llm=llm,
+)
+
+delete_all_nodes()
+text_splitter = TokenTextSplitter(chunk_size=512, chunk_overlap=24)
+
+documents_text_chunks = []
+for content in contents:
+    documents_text_chunks.extend(text_splitter.split_text(content))
+
+documents = [Document(page_content=chunk) for chunk in documents_text_chunks]
+
+
+graph_documents_filtered = llm_transformer_filtered.convert_to_graph_documents(documents)
+graph.add_graph_documents(graph_documents_filtered, baseEntityLabel=True, include_source=True)
+
+
+vector_index = Neo4jVector.from_existing_graph( 
+    OpenAIEmbeddings(),
+    search_type="hybrid",
+    node_label="Document",
+    text_node_properties=["text"],
+    embedding_node_property="embedding"
+)
+
+class Entities(BaseModel):
+    """Identifying information about entities."""
+
+    names: List[str] = Field(
+        ...,
+        description="All the person, organization, or business entities that appear in the text",
+    )
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are extracting organization and person entities from the text.",
+        ),
+        (
+            "human",
+            "Use the given format to extract information from the following"
+            "input: {question}",
+        ),
+    ]
+)
+
+entity_chain = prompt | llm.with_structured_output(Entities)
+
+def remove_lucene_chars(text):
+    lucene_special_chars = r'[+\-&|!(){}\[\]^"~*?:\\/]'
+    cleaned_text = re.sub(lucene_special_chars, '', text)
+    return cleaned_text
+
+graph.query(
+    "CREATE FULLTEXT INDEX entity IF NOT EXISTS FOR (e:__Entity__) ON EACH [e.id]")
+
+def generate_full_text_query(input: str) -> str:
+    """
+    Generate a full-text search query for a given input string.
+
+    This function constructs a query string suitable for a full-text
+    search. It processes the input string by splitting it into words and 
+    appending a similarity threshold (~2 changed characters) to each
+    word, then combines them using the AND operator. Useful for mapping
+    entities from user questions to database values, and allows for some 
+    misspelings.
+    """
+    full_text_query = ""
+    words = [el for el in remove_lucene_chars(input).split() if el]
+    for word in words[:-1]:
+        full_text_query += f" {word}~2 AND"
+    full_text_query += f" {words[-1]}~2"
+    return full_text_query.strip()
+
+def structured_retriever(question: str) -> str:
+    """
+    Collects the neighborhood of entities mentioned
+    in the question
+    """
+    result = ""
+    entities = entity_chain.invoke({"question": question})
+    for entity in entities.names:
+        response = graph.query(
+            """CALL db.index.fulltext.queryNodes('entity', $query, 
+            {limit:2})
+            YIELD node,score
+            CALL {
+              MATCH (node)-[r:!MENTIONS]->(neighbor)
+              RETURN node.id + ' - ' + type(r) + ' -> ' + neighbor.id AS 
+              output
+              UNION
+              MATCH (node)<-[r:!MENTIONS]-(neighbor)
+              RETURN neighbor.id + ' - ' + type(r) + ' -> ' +  node.id AS 
+              output
+            }
+            RETURN output LIMIT 50
+            """,
+            {"query": generate_full_text_query(entity)},
+        )
+        result += "\n".join([el['output'] for el in response])
+    return result
+
+def retriever(question: str):
+    print(f"Search query: {question}")
+    structured_data = structured_retriever(question)
+    unstructured_data = [el.page_content for el in vector_index.similarity_search(question)]
+    final_data = f"""Structured data:
+{structured_data}
+Unstructured data:
+{"#Document ". join(unstructured_data)}
+    """
+    return final_data
+
+template1 = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question,
+in its original language.
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:"""  # noqa: E501
+CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(template1)
+
+def _format_chat_history(chat_history: List[Tuple[str, str]]) -> List:
+    buffer = []
+    for human, ai in chat_history:
+        buffer.append(HumanMessage(content=human))
+        buffer.append(AIMessage(content=ai))
+    return buffer
+
+_search_query = RunnableBranch(
+    (
+        RunnableLambda(lambda x: bool(x.get("chat_history"))).with_config(
+            run_name="HasChatHistoryCheck"
+        ),  
+        RunnablePassthrough.assign(
+            chat_history=lambda x: _format_chat_history(x["chat_history"])
+        )
+        | CONDENSE_QUESTION_PROMPT
+        | ChatOpenAI(temperature=0)
+        | StrOutputParser(),
+    ),
+    RunnableLambda(lambda x : x["question"]),
+)
+template = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+Use natural language and be concise.
+Answer:"""
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = (
+    RunnableParallel(
+        {
+            "context": _search_query | retriever,
+            "question": RunnablePassthrough(),
+        }
+    )
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+print(chain.invoke({"question": "How many person are there in graph?"}))
