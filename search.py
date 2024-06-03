bdafe231ad91d624ebc524ec8db52f99e6adb4ef
Kaifan Song <202200502068@stumail.sztu.edu.cn>
2024-06-03 19:37:30 +0800

    Add files via upload

--- search.py
+++ search.py
@@ -15,6 +15,8 @@ from langchain_core.runnables import (
     RunnableParallel,
     RunnablePassthrough,
 )
+import json
+from templates2 import template_grade, template_search, template1,template_entity
 from langchain_core.documents import Document
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.prompts.prompt import PromptTemplate
@@ -60,7 +62,7 @@ from langchain.text_splitter import TokenTextSplitter
 
 os.environ["NEO4J_URI"] = "bolt://localhost:7687"
 os.environ["NEO4J_USERNAME"] = "neo4j"
-os.environ["NEO4J_PASSWORD"] = "Sztu2024!"
+os.environ["NEO4J_PASSWORD"] = "skf707=="
 
 graph = Neo4jGraph()
 
@@ -83,15 +85,8 @@ if model_name == "GPT3":
     llm = ChatOpenAI(model="gpt-3.5-turbo-16k", temperature=0)
 elif model_name == "GPT4":
     llm = ChatOpenAI(model="gpt-4", temperature=0)
+    
 llm_transformer = LLMGraphTransformer(llm=llm)
-
-vector_index = Neo4jVector.from_existing_graph( 
-    OpenAIEmbeddings(),
-    search_type="hybrid",
-    node_label="Document",
-    text_node_properties=["text"],
-    embedding_node_property="embedding"
-)
 
 @contextmanager
 def scoped_session():
@@ -100,8 +95,7 @@ def scoped_session():
             yield db_session
         finally:
             db_session.close() 
-            
-
+        
 class Entities(BaseModel):
     """Identifying information about entities."""
 
@@ -110,21 +104,9 @@ class Entities(BaseModel):
         description="All the verb, noun or business entities that appear in the text",
     )
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are extracting noun and verb entities from the text.",
-        ),
-        (
-            "human",
-            "Use the given format to extract information from the following"
-            "input: {question}",
-        ),
-    ]
-)
+prompt_entity = ChatPromptTemplate.from_messages(template_entity)
 
-entity_chain = prompt | llm.with_structured_output(Entities)
+entity_chain = prompt_entity | llm.with_structured_output(Entities)
 
 def remove_lucene_chars(text):
     lucene_special_chars = r'[+\-&|!(){}\[\]^"~*?:\\/]'
@@ -162,7 +144,7 @@ def structured_retriever(question: str) -> str:
     for entity in entities.names:
         response = graph.query(
             """CALL db.index.fulltext.queryNodes('entity', $query, 
-            {limit:2})
+            {limit:2})  
             YIELD node,score
             CALL {
               MATCH (node)-[r:!MENTIONS]->(neighbor)
@@ -183,20 +165,11 @@ def structured_retriever(question: str) -> str:
 def retriever(question: str):
     print(f"Search query: {question}")
     structured_data = structured_retriever(question)
-    unstructured_data = [el.page_content for el in vector_index.similarity_search(question)]
     final_data = f"""Structured data:
 {structured_data}
-Unstructured data:
-{"#Document ". join(unstructured_data)}
     """
     return final_data
 
-template1 = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question,
-in its original language.
-Chat History:
-{chat_history}
-Follow Up Input: {question}
-Standalone question:"""  # noqa: E501
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(template1)
 
 def _format_chat_history(chat_history: List[Tuple[str, str]]) -> List:
@@ -220,24 +193,57 @@ _search_query = RunnableBranch(
     ),
     RunnableLambda(lambda x : x["question"]),
 )
-template = """Answer the question based only on the following context:
-{context}
+prompt_search = ChatPromptTemplate.from_template(template_search)
 
-Question: {question}
-Use natural language and be concise.
-Answer:"""
-prompt = ChatPromptTemplate.from_template(template)
-
-chain = (
+chain_search = (
     RunnableParallel(
         {
             "context": _search_query | retriever,
             "question": RunnablePassthrough(),
         }
     )
-    | prompt
+    | prompt_search
+    | llm
+    | StrOutputParser()
+)
+prompt_grade = ChatPromptTemplate.from_template(template_grade)
+chain_grade = (
+    RunnableParallel(
+        {
+            "expected_result": RunnablePassthrough(),
+            "coll_result": RunnablePassthrough(),
+        }
+    )
+    | prompt_grade
     | llm
     | StrOutputParser()
 )
 
-print(chain.invoke({"question": "如何快速搭建知识库并绑定提示词？"}))
+
+file_path="fibona-drop rag测试集.json"
+# try:
+#     with open(file_path, 'r', encoding='utf-8') as file:
+#         data = json.load(file)
+
+#     for item in data:
+#         question = item["question"]
+#         expected_result = item["expected_result"]
+        
+#         coll_result = chain_search.invoke({"question": question})
+        
+#         grade_result = chain_grade.invoke({"expected_result": expected_result, "coll_result": coll_result})
+        
+#         print(f"Question: {question}")
+#         print(f"Collected Result: {coll_result}")
+#         print(f"Grade Result: {grade_result}")
+#         print("\n" + "-"*50 + "\n")
+
+# except UnicodeDecodeError as e:
+#     print(f"Unicode decoding error: {e}")
+# except json.JSONDecodeError as e:
+#     print(f"JSON decoding error: {e}")
+# except Exception as e:
+#     print(f"An unexpected error occurred: {e}")
+coll_result = chain_search.invoke({"question":"如何使用本地pdf文件新建知识库?"})
+grade_result = chain_grade.invoke({"expected_result":"在知识库聊天界面，点击右上角 **“市场”** 按钮即可进入知识库市场弹窗，在这里你可以对知识库进行点收藏，收藏后使用知识库进行聊天，并且在提示词时可供绑定。", "coll_result": coll_result})
+print(coll_result,"\n\n",grade_result)
